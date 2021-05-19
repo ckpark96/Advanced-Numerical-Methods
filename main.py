56ca617287c86f372171a24832f5580fc6bb97d0
Changkyu Park <48991602+ckpark96@users.noreply.github.com>
2021-05-19 10:08:03 +0200

    Update main.py

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ import numpy as np
 import matplotlib.pyplot as plt
 import scipy.sparse as sp
 import numpy.linalg as la
+import math
 # import sympy as sy
 
 N = 300 # number of nodes in the domain
@@ -62,12 +63,12 @@ def vanLeer(a): # flux limiter
 
 dx = L/N
 T = L*10/ubar
-dt = 0.9*dx/ubar
+dt = 0.4*dx/ubar
 dtpT = L/ubar/dt
 x = np.linspace(dx/2,L-dx/2,N) # Uniform grid
 xnon = np.concatenate((np.linspace(dx/2,L/2,N-(int(N/3))), np.linspace(L/2, L-dx/2,int(N/3)))) # Non-uniform grid
 IC = f(x)*dx
-# print(IC.shape)
+
 # solution = BeamWarming_lin(IC,dx,dt,T)/dx
 # plt.plot(solution[:,-1],label="Beam Warming")
 # solution = Upwind_lin(IC,dx,dt,T)/dx
@@ -77,7 +78,6 @@ IC = f(x)*dx
 # plt.plot(solution[:,0],"k--",label="Exact")
 # plt.legend()
 # plt.show()
-
 
 
 ''' Acoustic '''
@@ -91,35 +91,75 @@ eigvec2 = np.array([[Z],[1]])
 R = np.hstack((eigvec1,eigvec2))
 
 
-def godunov_acous(IC,dx,dt,T,lamp,lamn): # first order upwind
-    N = IC.shape[2]
-    sol_p = np.zeros((IC.shape[2],int(T//dt)+1)) # rows: spatial, columns: temporal (+1 to account for t=0)
-    sol_u = np.zeros((IC.shape[2],int(T//dt)+1))
-    sol_p[:,0] = IC[0,:,:] # Add the initial condition
-    sol_u[:,0] = IC[1,:,:]
+def alpha1(p0,p1,u0,u1,Z):
+    return 1/(2*Z)*(-1*(p1-p0)+Z*(u1-u0))
+
+def alpha2(p0,p1,u0,u1,Z):
+    return 1/(2*Z)*((p1-p0)+Z*(u1-u0))    
+
+def godunov_acous(ICp,ICu,dx,dt,T,Z,lam1,lam2): # first order upwind
+    N = ICp.shape[0]
+    r1 = np.array([[-Z],[1]])
+    r2 = np.array([[Z],[1]])
+    sol_p = np.zeros((IC.shape[0],int(T//dt)+1)) # along rows: spatial adv, along columns: temporal adv (+1 to account for t=0)
+    sol_u = np.zeros((IC.shape[0],int(T//dt)+1))
+    sol_p[:,0] = ICp # Add the initial condition
+    sol_u[:,0] = ICu
 
     for n in range(1,int(T//dt)+1): # progress in time but excluding t=0
         for i in range(sol_p.shape[0]): # progress in space
-            sol_p[i,n] = sol_p[i,n-1]-dt/dx*(lamp*(sol_p[i,n-1]-sol_p[i-1,n-1])+lamn*(sol_p[(i+1)%N,n-1]-sol_p[i,n-1]))
-            sol_u[i,n] = sol_u[i,n-1]-dt/dx*(lamp*(sol_u[i,n-1]-sol_u[i-1,n-1])+lamn*(sol_u[(i+1)%N,n-1]-sol_u[i,n-1]))
             
+            alph1n = alpha1(sol_p[i-1,n-1],sol_p[i,n-1],sol_u[i-1,n-1],sol_u[i,n-1],Z) # alpha^1_i-0.5
+            alph2n = alpha2(sol_p[i-1,n-1],sol_p[i,n-1],sol_u[i-1,n-1],sol_u[i,n-1],Z) # alpha^2_i-0.5
+            alph1p = alpha1(sol_p[i,n-1],sol_p[(i+1)%N,n-1],sol_u[i,n-1],sol_u[(i+1)%N,n-1],Z) # alpha^1_i+0.5
+            alph2p = alpha2(sol_p[i,n-1],sol_p[(i+1)%N,n-1],sol_u[i,n-1],sol_u[(i+1)%N,n-1],Z) # alpha^2_i+0.5
+
+            Qprev = np.array([[sol_p[i,n-1]],[sol_u[i,n-1]]]) # Q^n_i
+            # Q = Qprev - dt/dx*(2*alph1n*r1 - 2*alph2p*r2)
+            Q = Qprev - dt/dx*(2*alph2n*r2 - 2*alph1p*r1)
+            sol_p[i,n] = Q[0,:]
+            sol_u[i,n] = Q[1,:]
+           
     return sol_p, sol_u
 
+def BeamWarming_acou(ICp,ICu,dx,dt,T,lam1,lam2): # second order upwind
+    A = np.array([[lam1, 0],[0, lam2]])
+    sol_p = np.zeros((IC.shape[0],int(T//dt)+1)) # along rows: spatial adv, along columns: temporal adv (+1 to account for t=0)
+    sol_u = np.zeros((IC.shape[0],int(T//dt)+1))
+    sol_p[:,0] = ICp # Add the initial condition
+    sol_u[:,0] = ICu
 
+    for n in range(1,int(T//dt)+1): 
+        for i in range(sol_p.shape[0]):
+            Qprev = np.array([[sol_p[i,n-1]],[sol_u[i,n-1]]])
+            Qprevprev = np.array([[sol_p[i-1,n-1]],[sol_u[i-1,n-1]]])
+            Qprevprevprev = np.array([[sol_p[i-2,n-1]],[sol_u[i-2,n-1]]])
+            first = 3*Qprev - 4*Qprevprev + Qprevprevprev
+            second =  Qprev - 2*Qprevprev + Qprevprevprev
+            Q = Qprev - dt/2/dx*A@first + 0.5*(dt/dx)**2*A**2@second
+            sol_p[i,n] = Q[0,:]
+            sol_u[i,n] = Q[1,:]
 
-IC2 = np.array([[f(x)*dx],[np.ones_like(f(x))*dx]])
+    return sol_p, sol_u
 
-sol_p, sol_u = godunov_acous(IC2,dx,dt,T,lam_pos,lam_neg)
-sol_p = sol_p/dx
-print(sol_p.shape)
-sol_u = sol_u/dx
+ICp = f(x)
+ICu = np.ones_like(ICp)
 
-# plt.figure()
-# plt.title('p')
-# plt.plot(sol_p[:,-1],label="Godunov")
+sol_p_god, sol_u_god = godunov_acous(ICp,ICu,dx,dt,T,Z,lam_pos,lam_neg)
+sol_p_beam, sol_u_beam = BeamWarming_acou(ICp,ICu,dx,dt,T,lam_pos,lam_neg)
 
-# plt.figure()
-# plt.title('u')
-# plt.plot(sol_u[:,-1],label="Godunov")
+plt.figure()
+plt.title('p')
+plt.plot(sol_p_god[:,-1],label="Godunov")
+# plt.plot(sol_p_beam[:,50],label="Beam-Warming")
+plt.plot(sol_p_beam[:,-1],label="Beam-Warming")
+# plt.plot(sol_p[:,0],'k--',label='Exact')
+plt.legend()
+plt.figure()
+plt.title('u')
+plt.plot(sol_u_god[:,-1],label="Godunov")
+# plt.plot(sol_u_beam[:,50],label="Beam-Warming")
+plt.plot(sol_u_beam[:,-1],label="Beam-Warming")
+# plt.plot(sol_u[:,0],'k--',label='Exact')
 
-# plt.show()
+plt.show()
